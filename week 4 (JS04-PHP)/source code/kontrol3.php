0fa12184de41202f158217faad273ce5f25b24e3
rankadian <dianthafafian@gmail.com>
2024-09-17 10:58:20 +0700

    question 3 practical section 4

---
+++ week 4 (JS04-PHP)/source code/kontrol3.php
@@ -0,0 +1,10 @@
+<?php
+$points = 520;
+
+echo "Player's total score is: $points\n";
+
+if ($points > 500) {
+    echo "Do players get additional rewards? YES";
+} else {
+    echo "Do players get additional rewards? NO";
+}
