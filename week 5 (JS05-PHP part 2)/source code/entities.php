de6d85926074fe5879d1f456853320a0344ecb62
rankadian <dianthafafian@gmail.com>
2024-09-24 10:40:44 +0700

    Practical Section 9 (JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/entities.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Entities HTML</title>
+</head>
+
+<body>
+    <p>It's time to read a HTML book.</p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;Keuntungan dari menggunakan entitas adalah browser mungkin tidak mendukung karakter menggunakan nama entitas: Browser mungkin tidak mendukung nama entitas, tetapi dukungan untuk nomor lebih baik.</p>
+    <p>© 2023 jti.com</p>
+</body>
+
+</html>
