f6da41fa845751ca0dc3452f3527f5ae67043b17
rankadian <dianthafafian@gmail.com>
2024-09-24 10:47:11 +0700

    Practical Section 10 (JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/time.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+</head>
+
+<body>
+    <h3>Time</h3>
+    <?php
+    date_default_timezone_set("Asia/Jakarta");
+    echo date("H:i:s");
+    ?>
+</body>
+
+</html>
