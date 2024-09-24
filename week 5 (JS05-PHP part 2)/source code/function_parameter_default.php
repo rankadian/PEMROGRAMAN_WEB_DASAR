148f8d8a0c3b5524da6c26e4289702dd4b801deb
rankadian <dianthafafian@gmail.com>
2024-09-24 09:40:32 +0700

    Practical Section 4 part 3 (JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/function_parameter_default.php
@@ -0,0 +1,19 @@
+<?php
+//membuat fungsi
+function perkenalan($nama, $salam = "Assalamualaikum")
+{
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan("Diantha", "Hallo");
+
+echo "<hr>";
+
+$saya = "Evan";
+$ucapanSalam = "Selamat pagi";
+
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
