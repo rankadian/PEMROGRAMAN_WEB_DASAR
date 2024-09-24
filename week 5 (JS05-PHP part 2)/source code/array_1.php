42568fe1a18cc529ebeec543dcd35f466112496c
rankadian <dianthafafian@gmail.com>
2024-09-24 09:17:03 +0700

    Practical Section 1. Indexed Array (JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/array_1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Terindeks</title>
+</head>
+
+<body>
+    <h2>Array Terindeks</h2>
+    <?php
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pemenang", "Bagas Nugraha"];
+
+    echo $Listdosen[2] . "<br>";
+    echo $Listdosen[0] . "<br>";
+    echo $Listdosen[1] . "<br>";
+    ?>
+</body>
+
+</html>
