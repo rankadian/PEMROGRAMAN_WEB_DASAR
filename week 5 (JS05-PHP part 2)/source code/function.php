8728050f354301916abce36cfb4bc91fbe5c2de9
rankadian <dianthafafian@gmail.com>
2024-09-24 09:22:38 +0700

    Practical Section 4 part 1 (JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/function.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan()
+{
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Evan<br/>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
