236ea816d1de9c7ecdc5450db621489e3b50c7bf
rankadian <dianthafafian@gmail.com>
2024-09-24 10:12:53 +0700

    Practical Section 6 part 2 (JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/multi_menu_part2.php
@@ -0,0 +1,45 @@
+<?php
+function tampilkanMenuBertingkat($menu)
+{
+    echo "<ul>";
+    foreach ($menu as $item) {
+        echo "<li>" . $item['nama'] . "</li>";
+        if (isset($item['sub'])) {
+            tampilkanMenuBertingkat($item['sub']);
+        }
+    }
+    echo "</ul>";
+}
+
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "sub" => [
+            [
+                "nama" => "Wisata",
+                "sub" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner",
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
+
+tampilkanMenuBertingkat($menu);
