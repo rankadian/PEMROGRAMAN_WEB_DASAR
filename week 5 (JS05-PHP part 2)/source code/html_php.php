9abd5b729379fff7bd4f817b44feeb7d870207ad
rankadian <dianthafafian@gmail.com>
2024-09-24 10:34:52 +0700

    Practical Section 8 (JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/html_php.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cara 01</title>
+</head>
+
+<body>
+    <p>Tanggal hari ini : <?php echo date("d M Y") ?></p>
+</body>
+
+</html>
---
+++ week 5 (JS05-PHP part 2)/source code/html_php_part2.php
@@ -0,0 +1,7 @@
+<?php
+echo '<html>';
+echo '<head><title>Cara02</title></head>';
+echo '<body>';
+echo '<p>Tanggal Hari ini : ' . date('D M Y') . '</p>';
+echo '</body>';
+echo '</html>';
