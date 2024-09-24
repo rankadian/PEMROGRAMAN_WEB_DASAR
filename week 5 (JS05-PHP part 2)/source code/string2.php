40b780344413233d9bc7e280b7daacc73c51291d
rankadian <dianthafafian@gmail.com>
2024-09-24 10:28:16 +0700

    Practical Section 7(JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/string2.php
@@ -0,0 +1,8 @@
+<?php
+echo "Baris baru <br>";
+echo "Baris_baru <br>";
+echo "Halo Dunia <br>";
+
+echo "<pre>Halo Dunia</pre>";
+echo "<pre>Halo Dunia</pre>";
+echo "Katakanlah \"Tidak pada narkoba!\" <br>";
