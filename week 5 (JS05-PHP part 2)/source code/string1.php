a559d69d43c1d7cd5619ab6c49710b55966179f8
rankadian <dianthafafian@gmail.com>
2024-09-24 10:28:09 +0700

    Practical Section 7(JS05)

---
+++ week 5 (JS05-PHP part 2)/source code/string1.php
@@ -0,0 +1,9 @@
+<?php
+$loremIpsum = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quo non ex doloremque? Qui accusamus saepe voluptatibus itaque ex architecto modi, corrupti ratione! Qui sed harum sequi fuga, architecto eaque.";
+
+echo "<p>" . $loremIpsum . "</p>";
+
+echo "Panjang karakter: " . strlen($loremIpsum) . "<br>";
+
+echo "<p>" . strtoupper($loremIpsum) . "</p>";
+echo "<p>" . strtolower($loremIpsum) . "</p>";
