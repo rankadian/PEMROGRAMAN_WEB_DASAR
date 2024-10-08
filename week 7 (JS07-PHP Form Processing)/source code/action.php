4a13d3aa0d03bc39aab5854ab1e68450adde478c
rankadian <dianthafafian@gmail.com>
2024-10-08 11:36:05 +0700

    change action

--- week 7 (JS07-PHP Form Processing)/source code/action.php
+++ week 7 (JS07-PHP Form Processing)/source code/action.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Action for html_safe</title>
-  </head>
-  <body>
-    <form action="form_self.php" method="post">
-      <label for="input">Input:</label>
-      <input type="text" name="input" />
-      <input type="submit" value="submit" />
-      <!-- <script>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Action for html_safe</title>
+</head>
+
+<body>
+  <form action="html_safe.php" method="post">
+    <label for="input">Input:</label>
+    <input type="text" name="input" />
+    <input type="submit" value="submit" />
+    <!-- <script>
         alert("You have been hacked!");
       </script> -->
-    </form>
-  </body>
+  </form>
+</body>
+
 </html>
--- week 7 (JS07-PHP Form Processing)/source code/action2.php
+++ week 7 (JS07-PHP Form Processing)/source code/action2.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Action for html_safe2</title>
-  </head>
-  <body>
-    <form action="form_self2.php" method="post">
-      <label for="email">Email:</label>
-      <input type="text" name="email" />
-      <input type="submit" value="submit" />
-    </form>
-  </body>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Action for html_safe2</title>
+</head>
+
+<body>
+  <form action="html_safe2.php" method="post">
+    <label for="email">Email:</label>
+    <input type="text" name="email" />
+    <input type="submit" value="submit" />
+  </form>
+</body>
+
 </html>
