a3ae48ecd00dee7e634b7eb8f79ce14a65e0f6f3
rankadian <dianthafafian@gmail.com>
2024-11-18 23:40:06 +0700

    update code index ajax

--- week 10 (JS10-Bootstrap)/source_code/10.2/index.php
+++ week 10 (JS10-Bootstrap)/source_code/10.2/index.php
@@ -1,3 +1,248 @@
+<!-- praktikum CRUD read -->
+<?php
+/*
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Data Anggota</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <h2>Data Anggota</h2>
+        <a href="create.php" class="btn-tambah">Tambah Anggota</a>
+        <?php
+        include('koneksi.php');
+
+        $query = "SELECT * FROM anggota ORDER BY id DESC";
+        $result = mysqli_query($koneksi, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            $no = 1;
+            echo "<table>";
+            echo "<tr><th>No</th><th>Nama</th><th>Jenis Kelamin</th><th>Alamat</th><th>No. Telp</th><th>Aksi</th></tr>";
+
+            while ($row = mysqli_fetch_array($result)) {
+                $kelamin = ($row['jenis_kelamin'] === 'L') ? 'Laki-Laki' : 'Perempuan';
+                echo "<tr>";
+                echo "<td>" . $no++ . "</td>";
+                echo "<td>" . $row['nama'] . "</td>";
+                echo "<td>" . $kelamin . "</td>";
+                echo "<td>" . $row['alamat'] . "</td>";
+                echo "<td>" . $row['no_telp'] . "</td>";
+                echo "<td>
+                        <a href='edit.php?id=" . $row['id'] . "'>Edit</a> |
+                        <a href='#' onclick='konfirmasiHapus(" . $row['id'] . ", \"" . $row['nama'] . "\")'>Hapus</a>
+                      </td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "<p>Tidak ada data.</p>";
+        }
+        mysqli_close($koneksi);
+        ?>
+    </div>
+
+    <script>
+        function konfirmasiHapus(id, nama) {
+            var konfirmasi = confirm("Apakah Anda yakin ingin menghapus data dengan Nama: " + nama + "?");
+            if (konfirmasi) {
+                window.location.href = "proses.php?aksi=hapus&id=" + id;
+            }
+        }
+    </script>
+</body>
+
+</html>
+*/
+?>
+
+<!-- praktikum CRUD menggunakan bootstrap -->
+<?php
+/*
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Data Anggota</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-4">
+        <h2>Data Anggota</h2>
+        <a class="btn btn-success mt-2" href="create.php">Tambah Data</a>
+        <br><br>
+        <?php
+        include('koneksi.php');
+        $query = "SELECT * FROM anggota order by id desc";
+        $result = mysqli_query($koneksi, $query);
+        ?>
+        <table class="table">
+            <thead class="thead-light">
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Jenis Kelamin</th>
+                    <th>Alamat</th>
+                    <th>No. Telp</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $no = 1;
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $kelamin = ($row["jenis_kelamin"] == 'L') ? 'Laki-Laki' : 'Perempuan';
+                ?>
+                    <tr>
+                        <td><?= $no++; ?></td>
+                        <td><?= $row["nama"]; ?></td>
+                        <td><?= $kelamin; ?></td>
+                        <td><?= $row["alamat"]; ?></td>
+                        <td><?= $row["no_telp"]; ?></td>
+                        <td>
+                            <a class="btn btn-primary" href="edit.php?id=<?= $row["id"]; ?>">Edit</a>
+                            <a class="btn btn-danger" href="#" data-toggle="modal" data-target="#hapusModal<?= $row["id"]; ?>">Hapus</a>
+                        </td>
+                    </tr>
+
+                    <div class="modal fade" id="hapusModal<?= $row["id"]; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Konfirmasi Hapus</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Apakah Anda yakin ingin menghapus data dengan nama <?= $row["nama"]; ?>?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <a class="btn btn-danger" href="proses.php?aksi=hapus&id=<?= $row["id"]; ?>">Hapus</a>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+*/
+?>
+
+
+<!-- praktikum CRUD menggunakan ajax step 6 -->
+<?php
+/*
+include 'auth.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD dengan Ajax</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="<?= $_SESSION['csrf_token'] ?? '' ?>">
+
+    <!-- Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <!-- DataTables -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="#" style="color: #fff;">CRUD dengan Ajax</a>
+    </nav>
+
+    <div class="container">
+        <div class="mt-4">
+            <h2 align="center" style="margin: 30px;">Data Anggota</h2>
+            <div class="card">
+                <div class="card-body">
+                    <form method="post" id="form-data">
+                        <div class="form-group row">
+                            <label for="nama" class="col-sm-3 col-form-label">Nama</label>
+                            <div class="col-sm-9">
+                                <input type="text" name="nama" id="nama" class="form-control" required="true">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Jenis Kelamin</label>
+                            <div class="col-sm-9">
+                                <input type="radio" name="jenis_kelamin" id="jenkel1" value="L" required="true"> Laki-Laki
+                                <input type="radio" name="jenis_kelamin" id="jenkel2" value="P"> Perempuan
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="alamat">Alamat</label>
+                            <textarea name="alamat" id="alamat" class="form-control" required="true"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="no_telp">No Telepon</label>
+                            <input type="number" name="no_telp" id="no_telp" class="form-control" required="true">
+                        </div>
+                        <div class="form-group">
+                            <button type="button" name="simpan" id="simpan" class="btn btn-primary">
+                                <i class="fa fa-save"></i> Simpan
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="data"></div>
+        </div>
+    </div>
+
+    <div class="text-center">&copy; <?php echo date('Y'); ?> Design Dan Pemrograman Web</div>
+
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            // Mengatur Token Keamanan
+            $.ajaxSetup({
+                headers: {
+                    'Csrf-Token': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $('.data').load('data.php');
+        });
+    </script>
+</body>
+
+</html>
+*/
+?>
+
+
+<!-- praktikum CRUD menggunakan ajax step 13 & 14 -->
 <?php
 include 'auth.php';
 ?>
